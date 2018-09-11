a76bacb5799720afb9a822b66474d0565c43c665
Temporary <Temporary@Paul-XPS>
2018-09-11 10:23:41 -0700

    Updated version to 1.0.2

--- gocardless.php
+++ gocardless.php
@@ -13,7 +13,7 @@ class Gocardless extends NonmerchantGateway
     /**
      * @var string The version of this gateway
      */
-    private static $version = '1.0.0';
+    private static $version = '1.0.2';
 
     /**
      * @var string The authors of this gateway
